94c07e2a4c14cb2dd499dd463c53564c34d66ec3
mkearney <kearneymw@missouri.edu>
2019-08-30 13:48:40 -0500

    check for python3

--- tests/testthat/test-googler.R
+++ tests/testthat/test-googler.R
@@ -1,6 +1,12 @@
 test_that("googler works", {
-  x <- googler("hello")
-  expect_true(is.data.frame(x))
-  expect_true(nrow(x) > 1)
-  expect_true(all(c("url", "abstract") %in% names(x)))
+
+
+  if (Sys.which("python3") == "") {
+    expect_error(googler("hello"))
+  } else {
+    x <- googler("hello")
+    expect_true(is.data.frame(x))
+    expect_true(nrow(x) > 1)
+    expect_true(all(c("url", "abstract") %in% names(x)))
+  }
 })
