659171f0590eb50ea5cc974d5f1ae273ea12044f
Michael W. Kearney <kearneymw@missouri.edu>
2019-08-31 19:04:43 -0500

    test check for python not python3

--- tests/testthat/test-googler.R
+++ tests/testthat/test-googler.R
@@ -1,7 +1,7 @@
 test_that("googler works", {
   path <- Sys.which(paste0("python",
     c("", "3", "3.6", "3.7", "3.8", "3.9", "4", "3.4", "3.5")))
-  if (!any(grepl("python3", path))) {
+  if (!any(grepl("python", path))) {
     expect_error(googler("hello"))
   } else {
     x <- googler("hello")
